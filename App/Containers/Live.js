ffe9f1a53281d38e74947328a1b82b09f8cc85fc
jeremieo <jbanse3@mbp-de-jeremie.home>
2017-04-29 17:53:59 +0200

    Test Getting Live Data From FireBase

--- App/Containers/Live.js
+++ App/Containers/Live.js
@@ -1,39 +1,103 @@
 import React from 'react'
-import { ScrollView, Text } from 'react-native'
+import { ScrollView, Text, Image, View, TouchableOpacity } from 'react-native'
+import { DefaultRenderer, Actions as NavigationActions } from 'react-native-router-flux'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { Images, Colors, Metrics } from '../Themes'
 import { connect } from 'react-redux'
-// Add Actions - replace 'Your' with whatever your reducer is called :)
-// import YourActions from '../Redux/YourRedux'
-
+import LiveActions from '../Redux/LiveRedux'
+import { Screen, ListView, Tile, Title, Subtitle, Divider, Heading, Caption, Video } from '@shoutem/ui';
+import _ from 'lodash'
 // Styles
 import styles from './Styles/LiveStyle'
 
 class Live extends React.Component {
 
-   constructor (props) {
-     super(props)
- this.state = {
-  teamHome: 'Test',
-  teamAway: 'Présentation Live'
-              }
- }
+  constructor (props) {
+    super(props)
+    this.renderRow = this.renderRow.bind(this);
+    this.state = {
+      restaurants: [{
+        "name": "Gaspar Brasserie",
+        "address": "185 Sutter St, San Francisco, CA 94109",
+        "image": { "url": "https://shoutem.github.io/restaurants/restaurant-1.jpg" },
+      }, {
+        "name": "Chalk Point Kitchen",
+        "address": "527 Broome St, New York, NY 10013",
+        "image": { "url": "https://shoutem.github.io/restaurants/restaurant-2.jpg" },
+      }],
+    }
+  }
+
+  componentDidMount () {
+    this.props.getLivesRequest()
+  }
+
+  renderRow(live) {
+    return (
+      <View>
+
+      <Divider styleName="section-header">
+        <Caption>Match</Caption>
+      </Divider>
+<Video
+    source={{ uri: 'https://www.youtube.com/watch?v=B--D0eWkJ9s&feature=iv&src_vid=kps2HtNKR8U&annotation_id=channel%3A5901443b-0000-25fa-b412-94eb2c059ef0' }}
+    height={200}
+    width={300}
+/>
+
+
+          <Tile>
+
+<Title styleName="md-gutter-bottom">{live.teamHome} - {live.teamAway}</Title>
+          </Tile>
+
+
+
+      </View>
+
+    );
+  }
+
+
+
 
   render () {
+      console.tron.log(this.props)
     return (
-      <ScrollView style={styles.container}>
-        <Text>LiveOne Container {this.state.teamAway}</Text>
+            <View style={{flex: 1}}>
+                <Text>HELLO</Text>
+                      <Title>HELLO</Title>
+
+      <ScrollView style={styles.mainContainer}>
+
+          <View style={styles.container}>
+  {this.props.error && <Text style={{marginBottom: 20, color: 'red'}}>Il y à une erreur</Text> }
+          <Divider styleName="section-header">
+            <Caption>Debut Batard</Caption>
+          </Divider>
+            <ListView
+            data={_.values(this.props.lives)}
+            renderRow={this.renderRow}
+            />
+        </View>
       </ScrollView>
+</View>
+
+
     )
   }
 }
 
+
 const mapStateToProps = (state) => {
   return {
+    lives: state.live.lives,
+    error: state.live.error
   }
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-  }
-}
+const mapDispatchToProps = (dispatch) => ({
+  getLivesRequest: () => dispatch(LiveActions.getLivesRequest())
+})
 
 export default connect(mapStateToProps, mapDispatchToProps)(Live)
--- App/Containers/Styles/LiveStyle.js
+++ App/Containers/Styles/LiveStyle.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native'
-import { Colors, Metrics } from '../../Themes/'
+import { Colors,  Metrics, ApplicationStyles } from '../../Themes/'
 
 export default StyleSheet.create({
   container: {

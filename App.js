47067bd43c1a0e0b0b530cb91e55ae40639097a3
Fabien SAULNIER <f.saulnier@groupe-pomona.fr>
2018-09-16 23:29:43 +0200

    project structure

--- App.js
+++ App.js
@@ -3,5 +3,5 @@ import { useScreens } from 'react-native-screens';
 // Uncomment this to use react-native-screens
 // useScreens();
 
-import App from './js/App';
+import App from './app/App';
 export default App;
